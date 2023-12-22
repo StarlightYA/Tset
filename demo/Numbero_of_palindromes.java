370430c042a176955c724c302998777c4af79f58
StarlightYA <1799914786@qq.com>
2023-12-22 19:30:29 +0800

    Refactor palindrome checking logic in Numbero_of_palindromes.java

--- demo/Numbero_of_palindromes.java
+++ demo/Numbero_of_palindromes.java
@@ -1,18 +1,30 @@
 package demo;
 
 public class Numbero_of_palindromes {
-    //判断100-1000有多少个数字是回文数
+    //从键盘录入一个数判断是否是回文数
     public static void main(String[] args) {
-        int count = 0;
-        for (int i = 100; i < 1000; ++i) {
-            int a = i / 100;
-            int c = i % 10;
-            if (a == c) {
-                count++;
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        try {
+            System.out.println("请输入一个数：");
+            int num = s.nextInt();
+            int temp = num;
+            int sum = 0;
+            while (temp != 0) {
+                int ge = temp % 10;
+                sum = sum * 10 + ge;
+                temp /= 10;
             }
+            if (sum == num) {
+                System.out.println("是回文数");
+            } else {
+                System.out.println("不是回文数");
+            }
+        } finally {
+            s.close();
         }
-        System.out.println(count);
     }
+    
+    
     
     
 }
