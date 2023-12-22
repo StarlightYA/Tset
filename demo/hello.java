6eb317735786e12d0effc16a241675c54f9e2776
StarlightYA <1799914786@qq.com>
2023-12-22 11:48:33 +0800

    Add print statement to hello.java

--- demo/hello.java
+++ demo/hello.java
@@ -3,5 +3,6 @@ package demo;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
     }
 }
